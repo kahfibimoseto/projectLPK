7ce785d7a82ff34d411d597af5f6be9b184672db
kahfibimoseto <130158320+kahfibimoseto@users.noreply.github.com>
2023-05-09 08:37:34 +0700

    Update kimtikvolum.py

--- kimtikvolum.py
+++ kimtikvolum.py
@@ -68,7 +68,7 @@ with tab6:
     st.markdown("DATA BAHAN")
     namabahan=st.text_input("Nama Bahan Persen Volume")
     volumebahan=st.number_input("Volume Bahan(mL)", format = "%.3f")
-    volumebahan=st.number_input("VolumeLarutan(L)"), format = "%.3f")
+    volumebahan=st.number_input("VolumeLarutan(L)", format = "%.3f")
     if st.button('Persen Volume'):
         pvolume=((volumebahan)/(volumebahan)*100)
         st.info(f'Persen Volume dari {namabahan}: {pvolume}% (v/V)')
